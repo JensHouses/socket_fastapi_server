a946e367084eccb4b93436be1098ac0b59f7f5cb
JensHouses <82268654+JensHouses@users.noreply.github.com>
2023-04-12 16:47:27 +0200

    cleaned code and fixed sid debug msg

--- app/main.py
+++ app/main.py
@@ -1,25 +1,17 @@
 from fastapi import FastAPI, Body
 from socket_part import sio_app, sio
 
-
-# listen to all ips
-app = FastAPI(
-)
+app = FastAPI()
 app.mount("/socket.io", sio_app)
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
 
 
 @app.post("/broadcast/{channel}/{event}")
 async def login(channel: str, event: str,
                 data=Body(default={'data': 'to be send'}, description="Data to send")
                 ):
-    await sio.emit(event, {'data': data}, room=channel)
-    return {"Event": event,
-            "channel": channel,
-            "data": data
-            }
-
-
+    await sio.emit(event, data, room=channel)
+    return {
+        "Event": event,
+        "channel": channel,
+        "data": data
+    }
--- app/socket_part.py
+++ app/socket_part.py
@@ -14,37 +14,26 @@ sio_app = socketio.ASGIApp(
 
 @sio.event
 async def connect(sid, environ):
-    print("--'--"*20)
-    print('connection sid:', sid)
 
-    #await sio.save_session(sid, {'username': "Test username"})
-    await sio.emit('debug', {'status': 'Connected', sid: sid})
-
+    print('new connection. sid:', sid)
+    await sio.emit('debug', {'status': 'Connected', 'sid': sid})
     return "welcome"
 
 
 @sio.event
 async def disconnect(sid):
-    session = await sio.get_session(sid)
-    print("--'--"*20)
-    print(session.get("username", "--"), 'disconnect ', sid)
+    print('Session disconnected. sid:', sid)
     await sio.emit('debug', {'status': 'Disconnected', 'sid': sid})
 
 @sio.event
 async def subscribe(sid, data):
-    print("--'--"*20)
-    print("subscribe sid: ", sid)
-    print("subscribe data: ", data)
+    print("sid: ", sid, 'subscribed to', data)
     sio.enter_room(sid, data)
-    await sio.emit('debug', {'subscription_to': data, sid: sid})
+    await sio.emit('debug', {'subscription_to': data, 'sid': sid})
     return 'Entered room: ' + data
 
 @sio.event
 async def unsubscribe(sid, data):
-    print("--'--"*20)
-    print("unsubscribe sid: ", sid)
-    print("unsubscribe data: ", data)
+    print("sid: ", sid, 'unsubscribed to', data)
     sio.leave_room(sid, data)
     return 'Left room: ' + data
-
-
